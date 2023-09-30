bc1cacfe745ef0399f7562f52da75ce77a0d5678
Mônica Alcântara <monicaabrandao88@hotmail.com>
2023-09-30 19:07:54 -0300

    listagem da api

--- src/pages/Contacts/index.jsx
+++ src/pages/Contacts/index.jsx
@@ -1,24 +1,51 @@
+import api from "../../axios/axios";
+import { useEffect, useState } from "react"
 import { Link } from "react-router-dom"
 
 function Contacts(){
+    const [contacts, setContacts] = useState([]);
+
+    const getContacts = async() => {
+        try {
+            //biblioteca axios para utilizar a api
+            // const response = await axios.get('http://localhost:3000/contacts')
+            const response = await api.get('/contacts')
+
+            // console.log(response.data) preciso do data para acessar os dados da api:
+            const data = response.data
+            
+            //editar o contacts com informações da api
+            setContacts(data)
+
+        } catch (error) {
+            alert("falha ao encontrar contatos.")
+        }
+    } 
+
+    useEffect(() => {
+        getContacts(); 
+    }, [])
+
     return(
         <div>
+            <h1>Contatos</h1>
         <form>
             <input type="text" placeholder="Pesquisar pelo nome..." />
             <button>Pesquisar</button>
-        
-        <table>
-            <tbody>
-            <tr>
-                <td>Contatos</td>
-            </tr>
-            <tr>
-                <td>contato1</td>
-            </tr>
-            </tbody>
-        </table>
+            
+            <table>
+                <tbody>
+                {contacts.map(contact=>{return(
+                <tr key={contact.id}>
+                    <td>{contact.name} <button>editar</button></td>
+                </tr>
+                    )
+                })}
+                </tbody>
+            </table>
+         
         </form>
-        <Link to="/add-new-contact" className="btn">Adicionar novo contato</Link>
+        <Link to="/add-new-contact" className="btn ">Adicionar novo contato</Link>
         </div>
 
     )
