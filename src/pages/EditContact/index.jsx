1f24811c7f9e56209f947de355496bf203de8904
Mônica Alcântara <monicaabrandao88@hotmail.com>
2023-10-04 01:25:19 -0300

    edit working

--- src/pages/EditContact/index.jsx
+++ src/pages/EditContact/index.jsx
@@ -1,28 +1,43 @@
-import { useState } from "react"
-import { Link } from "react-router-dom"
+import {  useState } from "react"
+import { Link, useParams } from "react-router-dom"
+import api from "../../axios/axios";
 
-function EditdataEdit(){
+function EditContact(){
     const [name, setName] = useState()
     const [number, setNumber] = useState();
     const [address, setAddress] = useState();
+    const [contact, setContact] = useState({
+        id: 0,
+        name:"",
+        address:"",
+        number:"",
+    });
 
-// pesquise sobre userParams https://backefront.com.br/como-usar-useparams-react/
+    const params = useParams("/contacts/:id")
+    const {id} = params
 
-// vc precisa pegar seu server.json e ter ele aqui pra vc pesquisar nele
+    const getContact = async() => {
+        try {
+            const response = await api.get(`/contacts/${id}`)
+            setContact(response.data)
 
-// fazer uma busca nele onde o id que ta chegando seja igual o id do seu json
-// https://pt.stackoverflow.com/questions/238056/localizar-o-id-dentro-do-array-em-json
+        } catch (error) {
+            alert("falha ao encontrar contato.")
+        }
+    } 
 
-    // const handleEdit = async (e)=> {
-    //     e.preventDefault()
+    getContact()
+
+    const handleEdit = async (e)=> {
+        e.preventDefault()
         
-    //     await api.put(`/contacts/${id}`, {
-    //     id:0,
-    //     name:name,
-    //     address:address,
-    //     number:number,
-    //     })
-    // }
+        await api.put(`/contacts/${id}`, {
+        id:0,
+        name:name,
+        address:address,
+        number:number,
+        })
+    }
 
     return(
         <div>
@@ -30,20 +45,28 @@ function EditdataEdit(){
 
             <form action="">
                 <label>Nome:</label>
-                <input type="text" name="" id="" value={name} onChange={(e)=>setName(e.target.value)}/>
+                <input type="text" value={contact.name} onChange={(e)=>setName(e.target.value)}/>
 
                 <label>Endereço:</label>
-                <input type="text" name="" id="" value={address} onChange={(e)=>setAddress(e.target.value)}/>
+                <input type="text" value={contact.address} onChange={(e)=>setAddress(e.target.value)}/>
 
                 <label>Telefone:</label>
-                <input type="text" name="" id="" value={number} onChange={(e)=>setNumber(e.target.value)}/>
+                <input type="text" value={contact.number} onChange={(e)=>setNumber(e.target.value)}/>
 
                 <button onClick={handleEdit}>Salvar</button>
-                <Link to={"/"}>Voltar</Link> <button>Editar</button>
+                <Link to={"/"}>Voltar</Link>
 
             </form>
         </div>
     )
 }
 
-export default EditdataEdit
+export default EditContact
+
+
+// vc precisa pegar seu server.json e ter ele aqui pra vc pesquisar nele
+
+// fazer uma busca nele onde o id que ta chegando seja igual o id do seu json
+// https://pt.stackoverflow.com/questions/238056/localizar-o-id-dentro-do-array-em-json
+
+// pesquise sobre userParams https://backefront.com.br/como-usar-useparams-react/
